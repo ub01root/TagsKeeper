e1fe4776a263ae406a6d139fdead111486368a4e
DevAdvvy <ikaridevelopment@duck.com>
2026-06-29 08:25:42 -0500

    Refactor EconomyHook to PlaceholderAPIHook
    Refactor EconomyHook into PlaceholderAPIHook and implement placeholder request handling.

--- src/main/java/com/tagskeeper/hooks/PlaceholderAPIHook.java
+++ src/main/java/com/tagskeeper/hooks/PlaceholderAPIHook.java
@@ -1,75 +1,45 @@
 package com.tagskeeper.hooks;
 
 import com.tagskeeper.Main;
-import java.lang.reflect.Method;
-import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
+import com.tagskeeper.models.PlayerTagData;
+import com.tagskeeper.models.Tag;
+import com.tagskeeper.utils.ColorUtil;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.RegisteredServiceProvider;
+import org.jetbrains.annotations.NotNull;
 
-public class EconomyHook {
+public class PlaceholderAPIHook extends PlaceholderExpansion {
+   public @NotNull String getIdentifier() {
+      return "tagskeeper";
+   }
 
-   private Object economy;
-   private boolean enabled;
-   private Method hasMethod;
-   private Method withdrawMethod;
-   private Method formatMethod;
+   public @NotNull String getAuthor() {
+      return "DevAdvvy";
+   }
 
-   public boolean setup() {
-      if (Bukkit.getPluginManager().getPlugin("Vault") == null) {
-         Main.getInstance().getLogger().info("Vault not found, economy features disabled");
-         return false;
-      }
-      try {
-         Class<?> econClass = Class.forName("net.milkbowl.vault.economy.Economy");
-         RegisteredServiceProvider<?> rsp = Bukkit.getServicesManager().getRegistration(econClass);
-         if (rsp == null) {
-            Main.getInstance().getLogger().info("No economy provider found, economy features disabled");
-            return false;
+   public @NotNull String getVersion() {
+      return "1.0";
+   }
+
+   public boolean persist() {
+      return true;
+   }
+
+   public String onPlaceholderRequest(Player player, @NotNull String params) {
+      if (player == null) {
+         return "";
+      } else if (params.equalsIgnoreCase("tag")) {
+         PlayerTagData data = Main.getInstance().getPlayerDataManager().get(player.getUniqueId());
+         if (data == null) {
+            return "";
+         } else if (data.getSelectedTag() == null) {
+            return "";
+         } else {
+            Tag tag = Main.getInstance().getTagManager().getTag(data.getSelectedTag());
+            return tag == null ? "" : ColorUtil.color(tag.getTag());
          }
-         economy = rsp.getProvider();
-         hasMethod = econClass.getMethod("has", OfflinePlayer.class, double.class);
-         withdrawMethod = econClass.getMethod("withdrawPlayer", OfflinePlayer.class, double.class);
-         formatMethod = econClass.getMethod("format", double.class);
-         enabled = true;
-         Main.getInstance().getLogger().info("Hooked into Vault economy");
-         return true;
-      } catch (Exception e) {
-         Main.getInstance().getLogger().warning("Failed to hook Vault: " + e.getMessage());
-         return false;
-      }
-   }
-
-   public boolean isEnabled() {
-      return enabled;
-   }
-
-   public boolean hasBalance(Player player, double amount) {
-      if (!enabled) return false;
-      try {
-         return (boolean) hasMethod.invoke(economy, player, amount);
-      } catch (Exception e) {
-         return false;
-      }
-   }
-
-   public boolean withdraw(Player player, double amount) {
-      if (!enabled) return false;
-      try {
-         Object response = withdrawMethod.invoke(economy, player, amount);
-         Method successMethod = response.getClass().getMethod("transactionSuccess");
-         return (boolean) successMethod.invoke(response);
-      } catch (Exception e) {
-         return false;
-      }
-   }
-
-   public String format(double amount) {
-      if (!enabled) return String.valueOf(amount);
-      try {
-         return (String) formatMethod.invoke(economy, amount);
-      } catch (Exception e) {
-         return String.valueOf(amount);
+      } else {
+         return "";
       }
    }
 }
